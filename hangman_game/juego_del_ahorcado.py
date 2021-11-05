c780daaa650aab159c76b0295a6a5bb0d890048e
Fer Torres <fertorresmx@gmail.com>
2021-11-04 20:57:01 -0600

    Build pseudocode for hangman_game

---
+++ hangman_game/juego_del_ahorcado.py
@@ -0,0 +1,22 @@
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
+    # Read word's file
+    # Get one random word
+    # Print the game interface
+        #  Print blank spaces for the choosen word
+    # Get an input letter from the user
+        # Validate the input letter
+    # Compare the input with the letters from the choosen word
+        # Show in screen if the letter was correct or not:
+            # If is correct
+                # ...check if it's the last letter
+                    # ...if so, print WIN
+                # ...or print all the correct letters on the blank spaces
+            # but, if isn't correct, check the number of tries
+                # If is the last try,
+                    # ...print LOSE
+                # ...or ask for another letter (cicle)
